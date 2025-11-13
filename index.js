d1e5dac80cfac73705fdf752ba1e48ddd08b0bed
Noushin Anika Khan <noushinanikakhan@gmail.com>
2025-11-13 14:48:05 +0600

    added food and managemyfoods

--- index.js
+++ index.js
@@ -58,6 +58,25 @@ async function run() {
     })
 
 
+    // // ADD THIS ROUTE - Get single food by ID
+    // app.get('/foods/:id', async (req, res) => {
+    //   try {
+    //     const id = req.params.id;
+    //     const query = { _id: new ObjectId(id) }
+    //     const food = await foodsCollection.findOne(query);
+        
+    //     if (!food) {
+    //       return res.status(404).json({ message: 'Food not found' });
+    //     }
+        
+    //     res.send(food);
+    //   } catch (error) {
+    //     res.status(500).json({ message: error.message });
+    //   }
+    // })
+
+
+
     // PATCH - Update food (FIXED)
     app.patch('/foods/:id', async(req, res) => {
         try {
